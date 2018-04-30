d75de1dc8acf1fdddd857b14634b1260aeca90a2
Gergely Sánta <santa@eset.sk>
2018-04-30 20:41:21 +0200

    Base working version

--- MenuBarTimer/AppDelegate.swift
+++ MenuBarTimer/AppDelegate.swift
@@ -10,18 +10,68 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+	
 	@IBOutlet weak var window: NSWindow!
-
+	@IBOutlet weak var imageView: NSImageView!
+	
+	let statusItem = NSStatusBar.system.statusItem(withLength: 76)
+//	let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+		
+		// Customize status bar item
+		let timerProgress = ProgressImage(size: NSSize(width: 24.0, height: 16.0))
+		timerProgress.isTemplate = true
+		
+		statusItem.button?.image = timerProgress
+		statusItem.button?.title = "Timer"
+		statusItem.button?.imagePosition = .imageLeft
+		
+		// Add progress to main window
+		let progressImage = ProgressImage(size: NSSize(width: 100.0, height: 20.0))
+		
+		let fromColor = NSColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1.0)
+		var toColors:[NSColor] = [
+			NSColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0),
+			NSColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0),
+			NSColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+		]
+		var toColor:NSColor = toColors.removeFirst()
+
+		progressImage.progress = 0.0
+		progressImage.color = fromColor
+		
+		imageView.image = progressImage
+		
+		Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { (timer) in
+			if progressImage.progress == 1.0 {
+				if toColors.count > 0 {
+					toColor = toColors.removeFirst()
+					timerProgress.progress = 0.0
+					progressImage.progress = 0.0
+				}
+				else {
+					timer.invalidate()
+				}
+			}
+
+			timerProgress.progress += 0.001
+			self.statusItem.button?.title = String(format: "%.0f%%", timerProgress.progress*100)
+
+			progressImage.progress += 0.001
+			progressImage.color = NSColor(red: fromColor.redComponent + ((toColor.redComponent - fromColor.redComponent) * progressImage.progress),
+										  green: fromColor.greenComponent + ((toColor.greenComponent - fromColor.greenComponent) * progressImage.progress),
+										  blue: fromColor.blueComponent + ((toColor.blueComponent - fromColor.blueComponent) * progressImage.progress),
+										  alpha: fromColor.alphaComponent + ((toColor.alphaComponent - fromColor.alphaComponent) * progressImage.progress))
+			
+			self.imageView.needsDisplay = true
+			self.statusItem.button?.needsDisplay = true
+		}
 	}
-
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
+	
+	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+		return true
 	}
-
-
+	
 }
 
---
+++ MenuBarTimer/ProgressImage.swift
@@ -0,0 +1,104 @@
+//
+//  ProgressImage.swift
+//  MenuBarTimer
+//
+//  Created by Gergely Sánta on 30/04/2018.
+//  Copyright © 2018 TriKatz. All rights reserved.
+//
+
+import Cocoa
+import CoreGraphics
+
+class ProgressImage: NSImage {
+	
+	// MARK: - Public properties
+	
+	var cornerRadius:CGFloat = 5.0 {
+		didSet {
+			drawProgressBar()
+		}
+	}
+	
+	var backgroundOpacity:CGFloat = 0.6 {
+		didSet {
+			drawProgressBar()
+		}
+	}
+	
+	var color = NSColor.darkGray {
+		didSet {
+			if let srgbColor = color.usingColorSpace(.sRGB) {
+				let red = srgbColor.cgColor.components?[0] ?? 0.5
+				let green = srgbColor.cgColor.components?[1] ?? 0.5
+				let blue = srgbColor.cgColor.components?[2] ?? 0.5
+				let alpha = srgbColor.cgColor.alpha
+				progressColor = NSColor(red:red, green:green, blue:blue, alpha: alpha)
+				progressBackgroundColor = NSColor(red:red, green:green, blue:blue, alpha: alpha * backgroundOpacity)
+			}
+			else {
+				progressColor = NSColor(red:0.5, green:0.5, blue:0.5, alpha: 1.0)
+				progressBackgroundColor = NSColor(red:0.5, green:0.5, blue:0.5, alpha: backgroundOpacity)
+			}
+			drawProgressBar()
+		}
+	}
+	
+	var progress:CGFloat = 0.0 {
+		didSet {
+			if progress < 0.0 { progress = 0.0 }
+			else if progress > 1.0 { progress = 1.0 }
+			drawProgressBar()
+		}
+	}
+	
+	// MARK: - Private properties
+	
+	private var progressColor = NSColor.darkGray
+	private var progressBackgroundColor = NSColor.darkGray
+	
+	// MARK: - Initializers
+	
+	override init(size: NSSize) {
+		super.init(size: size)
+		initialize()
+	}
+	
+	required init(coder: NSCoder) {
+		super.init(coder: coder)
+		initialize()
+	}
+	
+	required init?(pasteboardPropertyList propertyList: Any, ofType type: NSPasteboard.PasteboardType) {
+		super.init(pasteboardPropertyList: propertyList, ofType: type)
+		initialize()
+	}
+	
+	private func initialize() {
+		self.color = NSColor.darkGray
+//		self.isTemplate = true
+	}
+	
+	// MARK: - Custom draw function
+	
+	private func drawProgressBar() {
+		let imgRect = NSRect(origin: CGPoint.zero, size: size)
+		
+		self.lockFocus()
+		
+		// Clear the image
+		imgRect.fill(using: NSCompositingOperation.clear)
+		
+		// Draw background
+		let backgroundPath = NSBezierPath(roundedRect: imgRect, xRadius: cornerRadius, yRadius: cornerRadius)
+		progressBackgroundColor.setFill()
+		backgroundPath.fill()
+		
+		// Draw progress bar
+		let progressPath = NSBezierPath(roundedRect: NSRect(origin: CGPoint.zero, size: CGSize(width: size.width*progress, height: size.height)), xRadius: cornerRadius, yRadius: cornerRadius)
+		progressColor.setFill()
+		progressPath.fill()
+		
+		self.unlockFocus()
+	}
+	
+}
