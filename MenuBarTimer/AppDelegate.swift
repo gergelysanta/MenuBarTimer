83f32d6f2c06a875298b48a5ea57e13545ff4975
Gergely Sánta <santa@eset.sk>
2018-04-30 15:47:41 +0200

    Initial Commit

---
+++ MenuBarTimer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenuBarTimer
+//
+//  Created by Gergely Sánta on 30/04/2018.
+//  Copyright © 2018 TriKatz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
